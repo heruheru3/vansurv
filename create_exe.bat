5a56777ba0eea3bcb52c37082992b26554d475af
Damisu <damiuoh@gmail.com>
2025-09-01 11:58:09 +0900

    追加ファイルの定義を更新

--- create_exe.bat
+++ create_exe.bat
@@ -1,1 +1,18 @@
-pyinstaller --noconfirm --onefile --windowed --icon "E:\jupy_work\vansurv\assets\favicon.ico" --add-data "E:\jupy_work\vansurv\__init__.py;." --add-data "E:\jupy_work\vansurv\constants.py;." --add-data "E:\jupy_work\vansurv\enemy.py;." --add-data "E:\jupy_work\vansurv\generate_icons.py;." --add-data "E:\jupy_work\vansurv\player.py;." --add-data "E:\jupy_work\vansurv\resources.py;." --add-data "E:\jupy_work\vansurv\subitems.py;." --add-data "E:\jupy_work\vansurv\ui.py;." --add-data "E:\jupy_work\vansurv\weapons;weapons/" --add-data "E:\jupy_work\vansurv\effects;effects/" --add-data "E:\jupy_work\vansurv\assets;assets/" --add-data "E:\jupy_work\vansurv\data;data/"  "E:\jupy_work\vansurv\main.py"
+pyinstaller --noconfirm --onefile --windowed ^
+--icon "E:\jupy_work\vansurv\assets\favicon.ico" ^
+--add-data "E:\jupy_work\vansurv\__init__.py;." ^
+--add-data "E:\jupy_work\vansurv\constants.py;." ^
+--add-data "E:\jupy_work\vansurv\enemy.py;." ^
+--add-data "E:\jupy_work\vansurv\generate_icons.py;." ^
+--add-data "E:\jupy_work\vansurv\player.py;." ^
+--add-data "E:\jupy_work\vansurv\resources.py;." ^
+--add-data "E:\jupy_work\vansurv\subitems.py;." ^
+--add-data "E:\jupy_work\vansurv\ui.py;." ^
+--add-data "E:\jupy_work\vansurv\collision.py;." ^
+--add-data "E:\jupy_work\vansurv\game_logic.py;." ^
+--add-data "E:\jupy_work\vansurv\game_utils.py;." ^
+--add-data "E:\jupy_work\vansurv\weapons;weapons/" ^
+--add-data "E:\jupy_work\vansurv\effects;effects/" ^
+--add-data "E:\jupy_work\vansurv\assets;assets/" ^
+--add-data "E:\jupy_work\vansurv\data;data/" ^
+"E:\jupy_work\vansurv\main.py"
